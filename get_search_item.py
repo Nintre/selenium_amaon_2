2233a203611d9015a8c5acf3264382b8d9943811
hutaiyi <313275847@qq.com>
2022-02-18 11:15:05 +0800

    添加clickhouse全局setting,和catch err

--- get_search_item.py
+++ get_search_item.py
@@ -1,13 +1,23 @@
+import os
+import sys
+import setting
 from clickhouse_driver import Client
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 def get_keywords(batch_size, n):
-    # connect ClickHouse, No need add port
-    client = Client(host='192.168.50.50', user='default', database='test')
+    try:
+        # connect ClickHouse, No need add port
+        client = Client(host=setting.CLICKHOUSE['HOSTNAME'], user=setting.CLICKHOUSE['USERNAME'],
+                        database=setting.CLICKHOUSE['DATABASE'])
 
-    sql_select = "select DISTINCT search_term from brand_analytics_search_terms_reports where department_name='Amazon.com' order by search_term limit {} offset {}".format(batch_size, n)
-    data_list = client.execute(sql_select)
-    for i in range(len(data_list)):
-        data_list[i] = (data_list[i][0])
+        sql_select = "select DISTINCT search_term from brand_analytics_search_terms_reports where department_name='Amazon.com' order by search_term limit {} offset {}".format(
+            batch_size, n)
+        data_list = client.execute(sql_select)
+        for i in range(len(data_list)):
+            data_list[i] = (data_list[i][0])
 
-    return data_list
+        return data_list
+    except Exception as e:
+        print('get_keywords fun err or connect clickhouse err:', e)
